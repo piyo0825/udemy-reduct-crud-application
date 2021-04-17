761bd5e0fc5fe9ca7d6e201a9ac4f48218171cad
piyo0825 <sachikokojima@gmail.com>
2021-04-17 18:18:26 +0900

    impliment Update Feature

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,6 +1,9 @@
 import _ from 'lodash'
 import { 
+	CREATE_EVENT,
 	READ_EVENTS,
+	READ_EVENT,
+        UPDATE_EVENT,
 	DELETE_EVENT,
 } from '../actions'
 
@@ -9,10 +12,15 @@ export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENTS: 
      return _.mapKeys(action.response.data, 'id')
+    case READ_EVENT:
+    case CREATE_EVENT:    
+    case UPDATE_EVENT:
+     const data = action.response.data
+     return { ...events, [data.id]: data }
     case DELETE_EVENT:
      delete events[action.id]
      return { ...events}
-    default:
+     default:
       return events
   }
 }
